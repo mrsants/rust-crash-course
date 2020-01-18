922c67220993f1fdad461eb6ac1682d0e8fda716
Marcelo Batista <marcelobatipta@gmail.com>
2020-01-18 15:23:56 -0300

    basic boolean

--- src/types.rs
+++ src/types.rs
@@ -24,14 +24,14 @@ pub fn run() {
     println!("Max i32: {}", std::i32::MAX);
     println!("Max i64: {}", std::i64::MAX);
   
-    // // Boolean
-    // let is_active: bool = true;
+    // Boolean
+    let is_active: bool = true;
   
-    // // Get boolean from expression
-    // let is_greater: bool = 10 < 5;
+    // Get boolean from expression
+    let is_greater: bool = 10 < 5;
   
-    // let a1 = 'a';
-    // let face = '\u{1F600}';
+    let a1 = 'a';
+    let face = '\u{1F600}';
   
-    // println!("{:?}", (x, y, z, is_active, is_greater, a1, face));
+    println!("{:?}", (x, y, z, is_active, is_greater, a1, face));
   }
