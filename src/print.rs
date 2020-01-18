05e291887823c9df80cdb19b6e78af877f535355
Marcelo Batista <marcelobatipta@gmail.com>
2020-01-18 14:46:54 -0300

    basic "interpolation"

--- src/print.rs
+++ src/print.rs
@@ -1,3 +1,23 @@
 pub fn run()  {
+  // Print console
   println!("Hello! World");
+
+  // Basic formatting
+  println!("{} is from {}", "Brad", "Mass");
+
+  // Basic formatting
+  println!("{0} is from {1} and {0} likes {2}" , "Brad", "Mass", "code");
+
+  // Named formatting
+  println!("{name} to play {activity}" , name = "John", activity = "Baseball");
+
+  // Placeholder 
+  println!("Binary: {:b} Hex: {:x} Octal: {:o}" , 10, 10, 10);
+
+  // Placeholder for debug trait 
+  println!("{:?}", (12, true, "hello"));
+
+  // Basic math 
+  println!("10 + 10 = {}", 10 + 10);
+ 
 }
