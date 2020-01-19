87afddc23646472a92acdd1a3d8d901314e3722a
Marcelo Batista <marcelobatipta@gmail.com>
2020-01-19 15:18:51 -0300

    Example string, vars

--- src/vars.rs
+++ src/vars.rs
@@ -5,20 +5,15 @@
 pub fn run() {
   let name = "Brad";
   let mut age = 37;
-
-  println!("My name is {} and {}", name, age);
-
+  println!("My name is {} and I am {}", name, age);
   age = 38;
+  println!("My name is {} and I am {}", name, age);
 
-  println!("My name is {} and {}", name, age);
-
-  // Define constant 
+  // Define constant
   const ID: i32 = 001;
-
   println!("ID: {}", ID);
 
-  // Assign multiple vars 
-  let (my_name, my_age ) = ("Brad", 37);
-
-  println!("{} is {}", my_name, my_age);
+  // Assign multiple vars
+  let ( my_name, my_age ) = ("Brad", 37);
+  println!("{} is {}", my_name, my_age );
 }
