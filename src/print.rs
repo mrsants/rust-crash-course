413ba4f70dc20bdf47c6079b3c98fbbf9ab0d871
Marcelo Batista <marcelobatipta@gmail.com>
2020-01-19 15:18:35 -0300

    Example point, print, string

--- src/print.rs
+++ src/print.rs
@@ -1,23 +1,29 @@
-pub fn run()  {
-  // Print console
-  println!("Hello! World");
+pub fn run() {
+  // Print to console
+  println!("Hello from the print.rs file");
 
-  // Basic formatting
+  // Basic Formatting
   println!("{} is from {}", "Brad", "Mass");
 
-  // Basic formatting
-  println!("{0} is from {1} and {0} likes {2}" , "Brad", "Mass", "code");
+  // Positional Arguments
+  println!(
+    "{0} is from {1} and {0} likes to {2}",
+    "Brad", "Mass", "code"
+  );
 
-  // Named formatting
-  println!("{name} to play {activity}" , name = "John", activity = "Baseball");
+  // Named Arguments
+  println!(
+    "{name} likes to play {activity}",
+    name = "John",
+    activity = "Baseball"
+  );
 
-  // Placeholder 
-  println!("Binary: {:b} Hex: {:x} Octal: {:o}" , 10, 10, 10);
+  // Placeholder traits
+  println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
 
-  // Placeholder for debug trait 
+  // Placeholder for debug trait
   println!("{:?}", (12, true, "hello"));
 
-  // Basic math 
+  // Basic math
   println!("10 + 10 = {}", 10 + 10);
- 
 }
